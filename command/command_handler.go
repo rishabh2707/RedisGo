76ce842af1e8a69b1fe97dda3650c1aac1467bf6
Rishabh Lakshkar <rishabh.lakshkar@gameberrylabs.com>
2025-11-03 11:18:04 +0530

    adding support of echo command

--- command/command_handler.go
+++ command/command_handler.go
@@ -1,11 +1,16 @@
 package command
 
-import "strings"
+import (
+	"strconv"
+	"strings"
+)
 
 func (cmnd *Cmd) Handle() string {
 	switch strings.ToUpper(cmnd.Name) {
 	case "PING":
 		return cmnd.handlePingCommand()
+	case "ECHO":
+		return cmnd.handleEchoCommand()
 	default:
 		return "-ERR unknown command '" + cmnd.Name + "'\r\n"
 	}
@@ -13,4 +18,8 @@ func (cmnd *Cmd) Handle() string {
 
 func (cmnd *Cmd) handlePingCommand() string {
 	return "+PONG\r\n"
+}
+
+func (cmnd *Cmd) handleEchoCommand() string {
+	return "$" + strconv.Itoa(len(cmnd.Args[1])) + "\r\n" + cmnd.Args[1] + "\r\n"
 }
