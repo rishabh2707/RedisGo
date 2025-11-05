d0067931242152f9bbc1cdaaf887e8353c79f263
Rishabh Lakshkar <rishabh.lakshkar@gameberrylabs.com>
2025-11-05 19:56:08 +0530

    adding support of custom port to start redis server

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
 	"os"
@@ -11,15 +12,20 @@ import (
 
 func main() {
 	fmt.Println("Welcome to RedisGo")
-	fmt.Println("Starting a tcp server on port 6379")
 
-	ln, err := net.Listen("tcp", "0.0.0.0:6379")
+	port := flag.String("port", "6379", "The port to listen on")
+
+	flag.Parse()
+
+	fmt.Println("Starting a tcp server on port " + *port)
+
+	ln, err := net.Listen("tcp", "0.0.0.0:"+*port)
 	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
+		fmt.Println("Failed to bind to port " + *port)
 		os.Exit(1)
 	}
 
-	fmt.Println("Server started on port 6379")
+	fmt.Println("Server started on port " + *port)
 
 	database.InitDataBase()
 
